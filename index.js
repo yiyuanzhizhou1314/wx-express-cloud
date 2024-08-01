b4660e81e0aa0e89362988f33d8be8caac588a0b
yiyuanzhizhou1314 <158150829+yiyuanzhizhou1314@users.noreply.github.com>
2024-08-01 14:12:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get("/api/count", async (req, res) => {
 // 小程序调用，获取微信 Open ID
 app.get("/api/wx_openid", async (req, res) => {
   if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
+    res.send('user openid'+req.headers["x-wx-openid"]);
   }
 });
 
